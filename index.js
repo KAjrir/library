c05cb768ba21d2d9c53258d1eb3dfff99425739c
Karim Ajrir <ajrir_karim@outlook.com>
2025-03-03 17:48:06 +0100

    reformed the code into classes

--- index.js
+++ index.js
@@ -1,65 +1,64 @@
- const inputButton = document.getElementById('inputform')
-
-const booklist = JSON.parse(localStorage.getItem("library")) || [];
-
-function Book(name, author, pages){
-    this.name = name
-    this.author = author
-    this.pages = pages
-};
-
-function saveBook(){
-    localStorage.setItem("library", JSON.stringify(booklist));
+class Book {
+    constructor(name, author, pages) {
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+    }
 }
 
+class Library {
+    constructor() {
+        this.booklist = JSON.parse(localStorage.getItem("library")) || [];
+    }
+
+    saveBooks() {
+        localStorage.setItem("library", JSON.stringify(this.booklist));
+    }
+
+    addBook(name, author, pages) {
+        const newBook = new Book(name, author, Number(pages));
+        this.booklist.push(newBook);
+        this.saveBooks();
+        this.updateList();
+    }
+
+    removeBook(index) {
+        this.booklist.splice(index, 1);
+        this.saveBooks();
+        this.updateList();
+    }
+
+    updateList() {
+        const bookContainer = document.querySelector('.book-container');
+        bookContainer.innerHTML = '';
+
+        this.booklist.forEach((book, index) => {
+            bookContainer.innerHTML += `
+                <div class="book-card">
+                    <p><strong>${book.name}</strong> by ${book.author}</p>
+                    <select name="status" id="status">
+                        <option value="not read yet">not read yet</option>
+                        <option value="reading">reading</option>
+                        <option value="read">read</option>
+                    </select>
+                    <div class="under-class">
+                        <button onclick='library.removeBook(${index})'>Delete</button>
+                        <p><strong>${book.pages} pages</strong></p>
+                    </div>
+                </div>`;
+        });
+    }
+}
+
+const library = new Library();
+library.updateList();
+
+const inputButton = document.getElementById('inputform');
 inputButton.addEventListener('submit', (event) => {
     event.preventDefault();
-    addBook();
-    inputButton.reset();
-
-})
-
-const bookContainer = document.querySelector('.book-container');
-
-function addBook(){
     const bookName = document.getElementById('name').value;
     const bookAuthor = document.getElementById('author').value;
     const bookPages = document.getElementById('pages').value;
-
-
-    const newBook = new Book(bookName, bookAuthor, Number(bookPages))
-
-    booklist.push(newBook)
-    saveBook();
-    updateList();
-}
-
-function removeBook(index){
-    booklist.splice(index, 1);
-    saveBook();
-    updateList();
-}
-
-function updateList(){
-    bookContainer.innerHTML = '';
-
-    booklist.forEach((book, index) => {
-        bookContainer.innerHTML += `
-            <div class="book-card">
-            <p><strong>${book.name}</strong> by ${book.author}</p>
-            <select name="status" id="status">
-                <option value="not read yet">not read yet</option>
-                <option value="reading">reading</option>
-                <option value="read">read</option>
-            </select>
-            <div class="under-class">
-                <button onclick='removeBook(${index})'>Delete</button>
-                <p><strong>${book.pages} pages</strong></p>
-            </div>
-        </div>`
-    })
-}
-
-
-
-updateList()
+    library.addBook(bookName, bookAuthor, bookPages);
+    inputButton.reset();
+});
